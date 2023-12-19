ff1bbaa025ac344d13be954842f02b3c0e0c5c9e
Vadim <www.220391@gmail.com>
2023-12-19 21:19:02 +0200

    improve

--- script.js
+++ script.js
@@ -1,31 +1,12 @@
 
-let text = document.querySelector('.text');
-
 document.querySelector('.navigation').addEventListener("click", function (event) {
 
-  if (event.target.innerText == 'About me'){
+  if (event.target.innerText == 'About me') window.location.href = './index.html';  
 
-    text.style.opacity = 0;
-    setTimeout(function(){
-      window.location.href = './index.html';
-    },1200);
-  }
+  else if (event.target.innerText == 'My experience') window.location.href = './myExperience.html';
 
-  else if (event.target.innerText == 'My experience'){
-    
-    text.style.opacity = 0;
-    setTimeout(function(){
-      window.location.href = './myExperience.html';
-    },1200);
-  }
-
-  else if (event.target.innerText == 'My demo projects'){
-
-    text.style.opacity = 0;
-    setTimeout(function(){
-      window.location.href = './myDemoProjects.html';
-    },1200);
-  }
-
+  else if (event.target.innerText == 'My demo projects') window.location.href = './myDemoProjects.html';
 });
+
+
 
